842d5902172f455cca03376360fcc8df17d196f6
comethrusws <basabjha.dev@gmail.com>
2025-12-19 09:34:25 +0545

    solved: prisma inti and deps issues

--- src/libs/helpers.ts
+++ src/libs/helpers.ts
@@ -1,23 +1,21 @@
-import {faCar, faMotorcycle, faChevronRight} from "@fortawesome/free-solid-svg-icons";
-import mongoose from "mongoose";
+import { PrismaClient } from "@prisma/client";
 
-export async function connect() {
-  return mongoose.connect(process.env.MONGODB_URL as string);
-}
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-export const categories = [
-  {key:'cars', label:'Cars', icon: faCar},
-  {key:'bikes', label:'Motorbikes', icon: faMotorcycle},
-  {key:'others', label:'Others', icon: faChevronRight},
-  
-];
+export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    log: ["query"],
+  });
 
-export function formatMoney(amount: number): string {
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+
+export const formatMoney = (amount: number): string => {
   return 'रू' + new Intl.NumberFormat('en-NP', { style: 'currency', currency: 'NPR', minimumFractionDigits: 2 }).format(amount).replace('NPR', '');
-}
+};
 
-export function formatDate(date: Date):string {
+export const formatDate = (date: Date): string => {
   return date.toLocaleDateString() + ' ' + date.toLocaleTimeString();
-}
+};
 
 export const defaultRadius = 50 * 1000;
